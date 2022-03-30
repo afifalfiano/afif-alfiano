3fbcadc5b11032a5e7dd8afc892a0b81ca6ffa9e
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-30 12:44:02 +0700

    fix: featured post

--- components/own/FeaturedPost.tsx
+++ components/own/FeaturedPost.tsx
@@ -1,5 +1,6 @@
 import Post from '../../types/post'
 import CardPost from '../../components/own/CardPost';
+import Link from 'next/link';
 
 type Props = {
     allPosts: Post[]
@@ -21,8 +22,14 @@ const FeaturedPost  = ({allPosts}: Props) => {
         <h1 className='text-8xl  pb-8 font-bold text-white text-center'>Featured Post</h1>
         <p className='text-2xl  pb-10 text-white text-center'>This is my blog or tutorial that make other people can read and implements some tutorial.</p>
 
-        <div className='flex gap-8 justify-center item-center'>
+        <div className='flex gap-10 justify-center item-center'>
             {featuredPost}
+        </div>
+
+        <div className='flex justify-center items-center mt-10'>
+            <Link href='/blogs'>
+                <button className='rounded-lg text-white font-bold py-2 px-4 bg-gradient-to-r from-yellow-500 to-orange-500 hover:from-pink-500 hover:to-yellow-500'>All Posts</button>
+            </Link>
         </div>
 
         </div>
