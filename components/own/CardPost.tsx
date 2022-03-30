7fe5c9a873e96b9523b8cb59dd05e5d8c2be14bf
Afif Alfiano <afif.alfiano@uii.ac.id>
2022-03-30 20:45:31 +0700

    fix: add time to read

--- components/own/CardPost.tsx
+++ components/own/CardPost.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 
@@ -6,19 +7,25 @@ const CardPost = ({ post }: any) => {
     const timePublish = new Date(post.date).toLocaleDateString('id-ID');
 
     return (
-        <div key={post.slug} className="flex flex-col  w-full bg-white rounded-lg ">
+        <Link href={'/posts/' + post.slug}>
+        <motion.div  whileHover={{scale: 1.1, cursor: 'pointer'}} transition={{duration: .5}} key={post.slug} className="flex flex-col w-full bg-white rounded-lg ">
             <div className="h-52 w-auto"><img src={post.coverImage} alt={post.title} className="rounded-tl-lg rounded-tr-lg w-full h-full" /></div>
             <div className="p-5 flex flex-col">
                 <p className="h-16 font-semibold text-lg">{post.title}</p>
-                <p className="h-8 text-gray-500">Post on <span className="text-blue-600">{timePublish}</span> by <span className="text-blue-600">{post.author.name}</span></p>
-                <p className="h-28 text-left">{post.excerpt.slice(0, 200) + '...'}</p>
-                <Link href={'/posts/' + post.slug}>
-                    <button className="items-end rounded-lg text-white font-bold py-2 px-4 bg-gradient-to-r from-yellow-500 to-orange-500 hover:from-pink-500 hover:to-yellow-500">
+                <p className="h-8 text-gray-500">Post on <span className="text-blue-500">{timePublish}</span> by <span className="text-blue-500">{post.author.name}</span></p>
+                <p className="text-left h-36">{post.excerpt}</p>
+                <div className="flex justify-between items-center">
+                <button className="items-end rounded-lg text-white font-bold py-2 px-4 bg-gradient-to-r from-yellow-500 to-orange-500 hover:from-pink-500 hover:to-yellow-500">
                     Read more
                     </button>
-                    </Link>
+                    <p className="text-blue-500 font-semibold">
+                    8 minutes to read
+                    </p>
+                </div>
+                    
             </div>
-        </div>
+        </motion.div>
+        </Link>
     )
 }
 
--- components/own/Header.tsx
+++ components/own/Header.tsx
@@ -10,15 +10,16 @@ const Header = () => {
             <motion.div initial={{opacity: 0}} animate={{ opacity: 1}} transition={{delay: .3, duration: .7}} className="flex flex-col justify-center items-start flex-auto ">
                 <h2  className='text-5xl text-white  pb-8 font-semibold'>Frontend Developer</h2>
                 <h1 className='text-8xl text-white pb-8 font-bold'>Afif Alfiano</h1>
-                <p className='text-2xl text-white pb-10'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum inventore dolorum explicabo cupiditate molestiae et quis assumenda, minima hic consequuntur.</p>
-                <button className='rounded-lg text-white font-bold py-2 px-4 bg-gradient-to-r from-yellow-500 to-orange-500 hover:from-pink-500 hover:to-yellow-500 transition-all duration-200'>Concact Me</button>
+                <p className='text-2xl text-white pb-10'>Actually, I don't know what I'm doing for now. But I like to do this thing. Welcome to my personal web guys. Hopefully it is usefull for you.</p>
+                <button className='rounded-lg text-white font-bold py-2 px-4 bg-gradient-to-r from-yellow-500 to-orange-500 hover:from-pink-500 hover:to-yellow-500 transition-colors  duration-200 delay-200'>Concact Me</button>
             </motion.div>
-            <motion.div initial={{opacity: 0}} animate={{ opacity: 1}} transition={{delay: .3, duration: .7}} className='flex flex-auto '>
-                <img src="../assets/new-afif.png" alt="Afif" className="img w-full" />
+            <motion.div initial={{opacity: 0}} animate={{ opacity: 1}} transition={{delay: .3, duration: .7}} className='flex flex-auto lg:ml-32'>
+                <img src="../assets/new-afif.png" alt="Afif" className="img w-auto" width="200px"/>
             </motion.div>
             </div>
         </div>
     )
 }
+
 
 export default Header;
